90b076648c2ecb73c69b7c9f03279b77564559a1
okamniev <olaksandr.kamniev@gmail.com>
2025-12-05 11:29:16 +0100

    Initial project structure

---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardPanel from './../components/dashboard/DashboardPanel'
+
+export default function DashboardView() {
+  return (<div>
+    <DashboardPanel />
+  </div>);
+}
